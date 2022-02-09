f1de73a86852f46e2e1d205899ab4aa1fd771146
Matheus-Mazepa <mfmazepa@hotmail.com>
2022-02-09 17:16:08 -0300

    Creating weapon controller

---
+++ test_like_a_hero/app/controllers/weapon_controller.rb
@@ -0,0 +1,18 @@
+class WeaponController < ApplicationController
+  def index
+    @weapons = Weapon.all
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def delete
+    @weapon = Weapon.find(params[:id])
+    @weapon.destroy
+    flash[:success] = "The to-do item was successfully destroyed."
+    redirect_to weapon_url
+  end
+end
--- test_like_a_hero/config/routes.rb
+++ test_like_a_hero/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   resources :users, only: [:index, :create]
+
+  resources :weapon
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
